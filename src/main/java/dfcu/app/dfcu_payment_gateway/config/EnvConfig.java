eb110415407ade2482ee09ded6d6f970ac82de37
Lukman Bukenya <bukenyalukman@gmail.com>
2025-05-11 16:40:36 +0300

    Fixed Missing .env File during testing

--- src/main/java/dfcu/app/dfcu_payment_gateway/config/EnvConfig.java
+++ src/main/java/dfcu/app/dfcu_payment_gateway/config/EnvConfig.java
@@ -2,8 +2,10 @@ package dfcu.app.dfcu_payment_gateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-@PropertySource("classpath:.env")
+@Profile("!test") // Don't load in test profile
+@PropertySource(value = ".env", ignoreResourceNotFound = true)
 public class EnvConfig {
 } 
